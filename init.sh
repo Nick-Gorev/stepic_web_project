726e30b444f6cded9eca339f3b4e7dbcd80451c8
Nick <nickag.ru>
2016-03-03 17:21:07 +0300

    status=200

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ sudo rm /etc/nginx/sites-enabled/default
 #gunicorn -b 0.0.0.0:8080 hello:app -D
 #gunicorn -b 0.0.0.0:8000 ask.wsgi:application -D
 
-cd /home/box/web/ask
-gunicorn -b 0.0.0.0:8000 ask.wsgi:application -D
+#cd /home/box/web/ask
+#gunicorn -b 0.0.0.0:8000 ask.wsgi:application -D
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
