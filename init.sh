60e4762ab2f1c0225081c9a7ebe7321a49bb1690
Nick <nickag.ru>
2016-03-03 16:07:19 +0300

    first django

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
-#sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo rm /etc/nginx/sites-enabled/default
+
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 /box/web/hello:app -D
+#gunicorn -b 0.0.0.0:8080 hello:app -D
+#gunicorn -b 0.0.0.0:8000 ask.wsgi:application -D
+
+cd /home/box/web/ask
+gunicorn -b 0.0.0.0:8000 ask.wsgi:application -D
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
