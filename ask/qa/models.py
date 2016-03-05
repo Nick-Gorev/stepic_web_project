445cffdcc801a75e748f5afe6481b1afcfb931f1
Nick <nickag.ru>
2016-03-05 16:04:20 +0300

    mysql lesson ver1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,33 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# вопрос
+class Question(models.Model):
+    # заголовок вопроса
+    title = models.CharField(max_length=255)
+    # полный текст вопроса
+    text = models.TextField()
+    # дата добавления вопроса
+    added_at = models.DateTimeField(blank=True)
+    #рейтинг вопроса (число)
+    rating = models.IntegerField(default = 0)
+    #автор вопроса
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    #список пользователей, поставивших "лайк"
+    likes = models.ManyToManyField(User)
+
+# ответ
+class Answer(models.Model):
+    # текст ответа
+    text = models.TextField()
+    # дата добавления ответа
+    added_at = models.DateTimeField(blank=True)
+    # вопрос, к которому относится ответ
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    #автор ответа
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
